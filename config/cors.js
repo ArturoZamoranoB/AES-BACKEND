cc5c9a98fa0f475904abd6c42d6e58f1b8bc13d5
ArturoZamoranoB <arturozamoranobeltran9@gmail.com>
2024-12-05 03:42:00 -0700

    cors

--- config/cors.js
+++ config/cors.js
@@ -1,5 +1,8 @@
 const cors_ = {
-    allowed_origins: '*'
+    origin: 'https://aes-frontend-five.vercel.app', // URL de tu frontend
+    methods: 'GET,POST,PUT,DELETE', // Métodos HTTP permitidos
+    allowedHeaders: 'Content-Type,Authorization', // Headers permitidos
+    credentials: true, // Permite el uso de cookies o encabezados de autorización
 };
 
 module.exports = cors_;
